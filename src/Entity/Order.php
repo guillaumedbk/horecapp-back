8d19a6e99a6b0943a02781b16da1e76533eb0cf4
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-09-28 11:10:09 +0200

    feat(Order)/HORECAPP-4 Creation of the Order entity & repository

---
+++ src/Entity/Order.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Uid\UuidV4;
+
+class Order
+{
+    //Attributes
+    #[ORM\Id]
+    #[ORM\Column]
+    private string $id; //UUID
+    #[ORM\Column]
+    private \DateTime $orderedAt;
+    #[ORM\Column]
+    private int $price;
+
+    //Constructor
+    public function __construct(\DateTime $orderedAt, int $price)
+    {
+        $this->id = Uuid::v4();
+        $this->orderedAt = $orderedAt;
+        $this->price = $price;
+    }
+
+    //Getters
+    public function getId(): UuidV4|string
+    {
+        return $this->id;
+    }
+
+    public function getOrderedAt(): \DateTime
+    {
+        return $this->orderedAt;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+}
